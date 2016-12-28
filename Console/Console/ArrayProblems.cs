90389bfbcb5d0783255a80ac57ddf8502f2f13aa
jjmew <jjmew@JJ-STUDIO>
2016-12-27 18:32:33 -0800

    Added new solution for TwoSumSortedArray

--- Console/Console/ArrayProblems.cs
+++ Console/Console/ArrayProblems.cs
@@ -9,22 +9,24 @@ namespace Console
 {
     class ArrayProblems
     {
-
-
         /// <summary>
         /// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
-        /// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.Please note that your returned answers(both index1 and index2) are not zero-based.
+        /// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers(both index1 and index2) are not zero-based.
         /// You may assume that each input would have exactly one solution.
         /// Input: numbers={2, 7, 11, 15}, target=9
         /// Output: index1=1, index2=2
         /// </summary>
-        /// <param name="numbers"></param>
-        /// <param name="target"></param>
+        /// <remarks>This is not the optimal solution but this approach demonstrates the usage of binary search.</remarks>
+        /// <param name="numbers">The array containing the list of numbers.</param>
+        /// <param name="target">The sum to be found.</param>
         /// <returns></returns>
-        public int[] TwoSum(int[] numbers, int target)
+        public int[] TwoSumSortedArray(int[] numbers, int target)
         {
+            if( numbers == null)
+            {
+                throw new ArgumentNullException("The argument numbers can't be null");
+            }
             int[] result = new int[2];
-
             int lastValue = numbers[0];
             int instances = 0;
 
@@ -44,6 +46,52 @@ namespace Console
                     return result;
                 }
             }
+            return result;
+
+        }
+
+        /// <summary>
+        /// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+        /// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers(both index1 and index2) are not zero-based.
+        /// You may assume that each input would have exactly one solution.
+        /// Input: numbers={2, 7, 11, 15}, target=9
+        /// Output: index1=1, index2=2
+        /// </summary>
+        /// <remarks> This solution implements a linear algorithm. We will start with two pointers. One at the begining and one to the end. If the sum of those is greater than what we are looking for then we will move back once 
+        /// position the pointer at the end. If it is lower then we will advance the pointer at the begining by one spot. We will do this until we find that the expected sum or both pointers meet.</remarks>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSumSortedArrayV2(int[] numbers, int target)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("The argument numbers can't be null");
+            }
+            int[] result = new int[2];
+            int start = 0;
+            int end = numbers.Length - 1;
+            while (start < end)
+            {
+                int sum = numbers[start] + numbers[end];
+
+                if (sum == target)
+                {
+                    // Adding one to each index since the answer is not zero based
+                    result[0] = start + 1;
+                    result[1] = end + 1;
+                    return result;
+                }
+                else if (sum > target)
+                {
+                    end--;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+
             return result;
 
         }
@@ -92,14 +140,27 @@ namespace Console
             return previousMax;
         }
 
+        /// <summary>
+        /// This is simple implementation of a binary search on. 
+        /// </summary>
+        /// <param name="numbers">The array to be searched.</param>
+        /// <param name="value">The value to be found.</param>
+        /// <param name="rangeMin">The lower limit (zero based) of the range to be searched. Use 0 if you want to search from the begining.</param>
+        /// <param name="rangeMax">The top limit (zero based) of the range to be searched. Use numbers.Lenght if you want to search to the end.</param>
+        /// <returns>Returns the index (zero based) of the item if it found. It returns -1 otherwise.</returns>
         public int BinarySearch(int[] numbers, int value ,int rangeMin, int rangeMax)
-
         {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("The argument numbers can't be null");
+            }
+            else if( rangeMin < 0 || rangeMax > (numbers.Length-1))
+            {
+                throw new ArgumentNullException("The argument rangeMin can't be lower than 0 and the argument rangeMax can't be larger than the array size");
+            }
+
             int low = rangeMin;
             int high = rangeMax;
-
-            if (rangeMax > numbers.Length || rangeMin < 0)
-                return -1;
 
             while( low <= high)
             {
--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -11,20 +11,6 @@ namespace Console
         static void Main(string[] args)
         {
 
-            ArrayProblems t = new ArrayProblems();
-
-            //int[] num = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 5, 5, 6, 7, 8, 9, 10 };
-
-            //int a= t.BinarySearch(num, 1);
-            //a = t.BinarySearch(num, 0);
-            //a = t.BinarySearch(num, 5);
-            //a = t.BinarySearch(num, 10);
-
-
-            int[] num = { 5, 25, 75};
-
-            t.TwoSum(num, 100);
-
         }
     }
 }
